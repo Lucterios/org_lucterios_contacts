2d7ccd1906ce13420cb822e9bf14e75e10eb068d
lag <lag@69e63612-969a-4371-9866-b14aae95a0cb>
2012-03-01 16:36:50 +0000

    Ajout d'un outil pour promouvoir un contact (physique ou moral) en un fils d'heritage (ex: adhérent)
    git-svn-id: http://projets.lucterios.org/svn/extensions/org_lucterios_contacts@1620 69e63612-969a-4371-9866-b14aae95a0cb

--- personneAbstraite_APAS_show.mth.php
+++ personneAbstraite_APAS_show.mth.php
@@ -1,24 +1,23 @@
 <?php
+// 	This file is part of Lucterios/Diacamma, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
+// 	Thanks to have payed a retribution for using this module.
 // 
-//     This file is part of Lucterios.
+// 	Lucterios/Diacamma is free software; you can redistribute it and/or modify
+// 	it under the terms of the GNU General Public License as published by
+// 	the Free Software Foundation; either version 2 of the License, or
+// 	(at your option) any later version.
 // 
-//     Lucterios is free software; you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation; either version 2 of the License, or
-//     (at your option) any later version.
+// 	Lucterios/Diacamma is distributed in the hope that it will be useful,
+// 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 	GNU General Public License for more details.
 // 
-//     Lucterios is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
+// 	You should have received a copy of the GNU General Public License
+// 	along with Lucterios; if not, write to the Free Software
+// 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // 
-//     You should have received a copy of the GNU General Public License
-//     along with Lucterios; if not, write to the Free Software
-//     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-// 
-// 	Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY
-//  // Method file write by SDK tool
-// --- Last modification: Date 20 April 2010 16:20:02 By  ---
+// 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY// Method file write by SDK tool
+// --- Last modification: Date 01 March 2012 8:28:32 By  ---
 
 require_once('CORE/xfer_exception.inc.php');
 require_once('CORE/rights.inc.php');
@@ -78,6 +77,24 @@ $xfer_result->setDBObject($self,"portable", true,$posY,$posX+2);
 $xfer_result->setDBObject($self,"fax", true,$posY,$posX+4);
 $posY+=50;
 $xfer_result->setDBObject($self,"commentaire", true,$posY++,$posX,5);
+
+$DBcurrent=$self;
+while ($DBcurrent->getSon()!=null)
+	$DBcurrent=$DBcurrent->getSon();
+global $rootPath;
+if(!isset($rootPath)) $rootPath = "";
+require_once('CORE/extensionManager.inc.php');
+if (count(getDaughterClassesList($DBcurrent->extname.'/'.$DBcurrent->tblname,$rootPath))>0) {
+	$xfer_result->m_context['current_table']=$DBcurrent->__table;
+	$xfer_result->m_context['current_id']=$DBcurrent->id;
+	$xfer_result->m_context['origininal_action']=$xfer_result->m_extension."/".$xfer_result->m_action;
+
+	$lbl = new Xfer_Comp_Button('upClass');
+	$lbl->setAction(new Xfer_Action("_Promouvoir","up.png",'CORE','upRecordClass',FORMTYPE_MODAL,CLOSE_YES));
+	$lbl->setLocation($posX,$posY++,5);
+	$xfer_result->addComponent($lbl);
+}
+
 return $xfer_result;
 //@CODE_ACTION@
 }
