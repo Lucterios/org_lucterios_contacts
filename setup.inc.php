c63b4b08c5b5cfbe36fbd50ce1b826054535776b
lag <lag@69e63612-969a-4371-9866-b14aae95a0cb>
2012-05-24 09:07:47 +0000

    Pour mise à jours v1.5 rev4
    git-svn-id: http://projets.lucterios.org/svn/extensions/org_lucterios_contacts@1723 69e63612-969a-4371-9866-b14aae95a0cb

--- setup.inc.php
+++ setup.inc.php
@@ -17,7 +17,7 @@
 // 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // 
 // 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY// setup file write by SDK tool
-// --- Last modification: Date 11 April 2012 4:29:07 By  ---
+// --- Last modification: Date 24 May 2012 2:06:20 By  ---
 
 $extention_name="org_lucterios_contacts";
 $extention_description="Module de gestion des contacts physiques (hommes ou femmes){[newline]}ou moraux (entreprise, association, administration,...)";
@@ -28,7 +28,7 @@ $extension_libre=true;
 
 $version_max=1;
 $version_min=3;
-$version_release=4;
+$version_release=5;
 $version_build=698;
 
 $depencies=array();
