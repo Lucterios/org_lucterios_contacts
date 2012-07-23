1aa29e8dea1a8841b89b160afd012832c4080830
Laurent GAY <l.gay@sd-libre.fr>
2012-07-23 14:44:33 -0700

    Correction sur la recherche via selectionneur de contact

--- personneAbstraite_APAS_searchContact.mth.php
+++ personneAbstraite_APAS_searchContact.mth.php
@@ -1,23 +1,21 @@
 <?php
-// 	This file is part of Lucterios/Diacamma, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
-// 	Thanks to have payed a retribution for using this module.
+// This file is part of Lucterios, a software developped by "Le Sanglier du Libre" (http://www.sd-libre.fr)
+// Thanks to have payed a donation for using this module.
 // 
-// 	Lucterios/Diacamma is free software; you can redistribute it and/or modify
-// 	it under the terms of the GNU General Public License as published by
-// 	the Free Software Foundation; either version 2 of the License, or
-// 	(at your option) any later version.
+// Lucterios is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
 // 
-// 	Lucterios/Diacamma is distributed in the hope that it will be useful,
-// 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-// 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// 	GNU General Public License for more details.
+// Lucterios is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 // 
-// 	You should have received a copy of the GNU General Public License
-// 	along with Lucterios; if not, write to the Free Software
-// 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-// 
-// 		Contributeurs: Fanny ALLEAUME, Pierre-Olivier VERSCHOORE, Laurent GAY// Method file write by SDK tool
-// --- Last modification: Date 25 May 2012 4:26:44 By  ---
+// You should have received a copy of the GNU General Public License
+// along with Lucterios; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+// Method file write by Lucterios SDK tool
 
 require_once('CORE/xfer_exception.inc.php');
 require_once('CORE/rights.inc.php');
@@ -29,8 +27,9 @@ require_once('extensions/org_lucterios_contacts/personneAbstraite.tbl.php');
 //@DESC@
 //@PARAM@ posY
 //@PARAM@ xfer_result
+//@PARAM@ ExtractFields=array()
 
-function personneAbstraite_APAS_searchContact(&$self,$posY,$xfer_result)
+function personneAbstraite_APAS_searchContact(&$self,$posY,$xfer_result,$ExtractFields=array())
 {
 //@CODE_ACTION@
 if (isset($xfer_result->m_context['classname']))
